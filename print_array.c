7c768d412dcd501ce6611f0db4eef7e2df235599
KayKilb <infulbloom@msn.com>
2023-04-24 15:01:14 -0700

    created all initial files, and completed task 0

---
+++ print_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array __attribute__((unused)), size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
