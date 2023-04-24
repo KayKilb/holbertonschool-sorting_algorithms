ba9b651cb8513e84529d5c315fa263426d447513
KayKilb <infulbloom@msn.com>
2023-04-24 15:36:10 -0700

    updates

--- sort.h
+++ sort.h
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
- * @n: integer stored in the node
- * @prev: pointer to the previous element of the list
- * @next: pointer to the next element of the list
+ * struct listint_s - Linked lists
+ * @n: integer stored
+ * @prev: prev element
+ * @next: next element
  */
+
 typedef struct listint_s
 {
 	const int n;
@@ -17,17 +18,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Given functions */
-void print_array(const int *array, size_t size)
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/* aux functions */
 void swap_head(listint_t **list, listint_t *aux);
 void swap_middle(listint_t *aux);
 void swap_tail(listint_t *aux);
